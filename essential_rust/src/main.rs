874dbfbd6890278ce57fd42b313fe9d9353e51e0
Gian Santos <giansantos.carlos@gmail.com>
2026-02-02 21:35:00 -0300

    Better loop control in Rust

--- essential_rust/src/main.rs
+++ essential_rust/src/main.rs
@@ -1,14 +1,33 @@
 
 fn main() {
 
-    let mut n = 0;
-    loop {
-        n+=1;
-        println!("hello word {}", n)
-        
+
+
+    let mut main_count: i32 = 0;
+
+    'main: loop {
+        println!("Outer : {main_count}");
+        let mut inner_count: i32 = 0;
+
+
+        loop {
+            println!("inner : {inner_count}");
+            inner_count +=1;
+
+
+            if inner_count == 3 {
+                println!("---");
+            }
+
+            if main_count == 3 {
+                println!("Existing out os all loops ");
+                break 'main;
+            }
+            main_count += 1;
+        }
     }
 }
 
-option_env!()
+
 
 
